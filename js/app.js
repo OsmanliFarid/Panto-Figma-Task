4291ef0daae1ae7e63c902514b4e7da3a7f3779c
OsmanliFarid <osmanliferid686@gmail.com>
2025-04-24 16:19:46 +0400

    js change

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ let selectedId = 1
 const ShopIndex = document.querySelector("#ShopIndex")
 const ShowBtn = (selectId) => {
   selectedId = selectId 
-  ShopIndex.innerText++
+  
   show(arr1)
 }
 const show = (array) => {
@@ -40,3 +40,46 @@ const show = (array) => {
 }
 
 show(arr1)
+const MainProductListGarder = document.querySelector("#MainProductListGarder")
+
+const array2 = [
+    {id:10,img:"./images/productImage.png",title:"Sakarias Armchair",value:392,},
+    {id:10,img:"./images/productImage2.png",title:"Sakarias Armchair",value:299,},
+    {id:10,img:"./images/productImage3.png",title:"Sakarias Armchair",value:519,},
+    {id:10,img:"./images/productImage4.png",title:"Sakarias Armchair",value:921,},
+]
+const ShowChair = (arr3) =>{
+    MainProductListGarder.innerHTML = ""
+   arr3.forEach((item) =>{
+    MainProductListGarder.innerHTML += `<div class="MainProductChairBox">
+                            <img src="${item.img}" alt="" class="img3">
+                            <div class="MainProductChairBox2">
+                                <h3 class="MainProductChairTitle">Chair</h3>
+                            <h1 class="MainProductChairTitle2">${item.title}</h1>
+                            <img src="./images/star.png" alt="">
+                            <div class="MainProductChairBox3">
+                                <h1>$'${item.value}'</h1>
+                                <h1 class="collect2">+</h1>
+                            </div>
+                            </div>
+                            
+                        </div>`
+                                      
+    
+   })
+   
+   const collect2 = document.querySelectorAll(".collect2")
+   collect2.forEach((item) =>{
+    item.addEventListener("click",() =>{
+        ShopIndex.innerText++
+        
+    })
+    
+   })
+   
+   
+   
+} 
+
+
+ShowChair(array2)
